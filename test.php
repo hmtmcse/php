de1bb970fd71d527b2e375a67cfc353ef64c9164
hmtmcse <hmtm.cse@gmail.com>
2021-01-06 20:36:02 +0600

    bismillah

--- test.php
+++ test.php
@@ -1,6 +1,19 @@
 <?php
 
-$arrayCheck = [];
-if (isset($arrayCheck)){
-    echo "Yessssss";
+//$arrayCheck = [];
+//if (isset($arrayCheck)){
+//    echo "Yessssss";
+//}
+
+$dripTTags = null;
+
+if (!is_array($dripTTags)){
+    $dripTTags = [];
 }
+
+$custom_fields = ['abc' => 'abc', 'xy' => 'xy'];
+$custom_fields = [];
+$dripTTags["custom_fields"]["verification_url"] = 'xyz_abcd';
+$dripTTags["custom_fields"] = array_merge($dripTTags["custom_fields"], $custom_fields);
+
+print_r($dripTTags);
